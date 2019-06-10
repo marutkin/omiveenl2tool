d142bc1415af745814bbff3ac25a69eb132cd383
Dmitriy Marutkin <dmitriy.marutkin@firstlinesoftware.com>
2019-06-10 16:06:53 +0300

    Initial - add project architecture

---
+++ l2tool/src/js/components/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import styles from './../../sass/styles.scss';
+
+function App() {
+  return (
+    <main className={styles["main-container"]}>
+      Simple-Text
+    </main>
+  );
+}
+
+export default App;
---
+++ l2tool/webpack.config.js
@@ -0,0 +1,118 @@
+
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = !process.env.production;
+const isProduction = process.env.production;
+const distPath = path.join(__dirname, '/public');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'main.css',
+  disable: isDevelopment
+});
+
+const config = {
+  entry: {
+    app: './src/js/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: distPath
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }]
+    }, {
+      test: /\.scss$/,
+      exclude: [/node_modules/],
+      use: extractSass.extract({
+        fallback: 'style-loader',
+        use: [{
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            sourceMap: true,
+            importLoaders: 2,
+            localIdentName: '[name]__[local]__[hash:base64:5]', // className template
+            minimize: isProduction
+          }
+        },
+          'sass-loader',
+          'resolve-url-loader'
+        ]
+      })
+    }, {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name][hash].[ext]'
+        }
+      }, {
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            progressive: true,
+            quality: 70
+          }
+        }
+      },
+      ],
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name][hash].[ext]'
+        }
+      },
+    }]
+  },
+  plugins: [
+    extractSass,
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  devServer: {
+    contentBase: distPath,
+    compress: true,
+    open: true,
+    port: 9000
+  }
+};
+
+if (isDevelopment) {
+  fs.readdirSync(distPath)
+    .map((fileName) => {
+      if (['.css', '.js'].includes(path.extname(fileName))) {
+        return fs.unlinkSync(`${distPath}/${fileName}`);
+      }
+
+      return '';
+    });
+}
+
+if (isProduction) {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    })
+  );
+}
+
+module.exports = config;
